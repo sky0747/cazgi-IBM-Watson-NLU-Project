293d4e646ad17419c67ed1b31ff09053c905fc2b
sky0747 <vova986@berdyansk.net>
2021-02-27 10:22:50 +0200

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeClient/src/App.js
+++ sentimentAnalyzeClient/src/App.js
@@ -45,9 +45,6 @@ class App extends React.Component {
     }
     ret = axios.get(url);
     ret.then((response)=>{
-
-      //Include code here to check the sentiment and fomrat the data accordingly
-
       this.setState({sentimentOutput:response.data});
       let output = response.data;
       if(response.data === "positive") {
@@ -55,7 +52,7 @@ class App extends React.Component {
       } else if (response.data === "negative"){
         output = <div style={{color:"red",fontSize:20}}>{response.data}</div>
       } else {
-        output = <div style={{color:"orange",fontSize:20}}>{response.data}</div>
+        output = <div style={{color:"yellow",fontSize:20}}>{response.data}</div>
       }
       this.setState({sentimentOutput:output});
     });
--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,5 +1,24 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
 const app = new express();
+
+function getNLUInstance(){
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+    
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2018-05-01',
+        authenticator: new IamAuthenticator({apikey: api_key,}),
+        serviceUrl: api_url,
+    });
+    return naturalLanguageUnderstanding;
+}
+
 
 app.use(express.static('client'))
 
@@ -11,23 +30,99 @@ app.get("/",(req,res)=>{
   });
 
 app.get("/url/emotion", (req,res) => {
-
-    return res.send({"happy":"90","sad":"10"});
+    let inputUrl = req.query.url;
+    console.log(inputUrl);
+    const analyzeParams = {
+      //'url': "https://newsroom.ibm.com/2021-02-15-bp-joins-the-IBM-Quantum-Network-to-advance-use-of-quantum-computing-in-energy",
+      'url': inputUrl,
+      'features': {
+        'emotion': {
+        }
+      },
+    };
+    const naturalLanguageUnderstanding = getNLUInstance();
+    naturalLanguageUnderstanding.analyze(analyzeParams)
+        .then(analysisResults => {
+            console.log(JSON.stringify(analysisResults, null, 2));
+            res.send(analysisResults.result.emotion.document.emotion);
+        })
+        .catch(err => {
+            console.log('error:', err);
+        });
+    //return res.send({"happy":"90","sad":"10"});
 });
 
 app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
+    let inputUrl = req.query.url;
+    console.log(inputUrl);
+    const analyzeParams = {
+      //'url': "https://newsroom.ibm.com/2021-02-15-bp-joins-the-IBM-Quantum-Network-to-advance-use-of-quantum-computing-in-energy",
+      'url': inputUrl,
+      'features': {
+        'sentiment': {
+        }
+      },
+    };
+    const naturalLanguageUnderstanding = getNLUInstance();
+    naturalLanguageUnderstanding.analyze(analyzeParams)
+        .then(analysisResults => {
+            console.log(JSON.stringify(analysisResults, null, 2));
+            var outputSent = analysisResults.result.sentiment.document.label;
+            res.send(outputSent);
+        })
+        .catch(err => {
+            console.log('error:', err);
+        });
+    //return res.send("url sentiment for "+req.query.url);
 });
 
 app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+    let inputText = req.query.text;
+    console.log(inputText);
+    const analyzeParams = {
+      //'text': "As much as I want to congratulate them, I feel sad.",
+      'text': inputText,
+      'features': {
+        'emotion': {
+        }
+      },
+    };
+    const naturalLanguageUnderstanding = getNLUInstance();
+    naturalLanguageUnderstanding.analyze(analyzeParams)
+        .then(analysisResults => {
+            console.log(JSON.stringify(analysisResults, null, 2));
+            res.send(analysisResults.result.emotion.document.emotion);
+        })
+        .catch(err => {
+            console.log('error:', err);
+        });
+    //return res.send({"happy":"10","sad":"90"});
 });
 
 app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+    let inputText = req.query.text;
+    console.log(inputText);
+    const analyzeParams = {
+      //'text': "As much as I want to congratulate them, I feel sad.",
+      'text': inputText,
+      'features': {
+        'sentiment': {
+        }
+      },
+    };
+    const naturalLanguageUnderstanding = getNLUInstance();
+    naturalLanguageUnderstanding.analyze(analyzeParams)
+        .then(analysisResults => {
+            console.log(JSON.stringify(analysisResults, null, 2));
+            var outputSent = analysisResults.result.sentiment.document.label;
+            res.send(outputSent);
+        })
+        .catch(err => {
+            console.log('error:', err);
+        });
+    //return res.send("text sentiment for "+req.query.text);
 });
 
-let server = app.listen(8080, () => {
+let server = app.listen(3000, () => {
     console.log('Listening', server.address().port)
 })
-
